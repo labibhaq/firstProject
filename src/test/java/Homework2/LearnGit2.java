d33a25ff29f93085c6866fa9279e02a80285d16f
labibhaq <43007819+labibhaq@users.noreply.github.com>
2024-07-04 19:06:23 -0500

    2nd commit

---
+++ src/test/java/Homework2/LearnGit2.java
@@ -0,0 +1,27 @@
+package Homework2;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void printMe1() {
+		System.out.println("git test");
+
+	}
+	
+	public void printMe2() {
+		System.out.println("git test");
+
+	}
+	
+	public void printMe3() {
+		System.out.println("git test");
+
+	}
+}
+}
+
+}
